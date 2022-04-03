8b4dacfd15d56b52f6c759b18680cf9d449b8d95
SimmiDhar <Simrandhar19@gmail.com>
2022-04-03 18:42:14 +0530

    first chekin

---
+++ Project_1/src/myPackage/MyClass.java
@@ -0,0 +1,19 @@
+package myPackage;
+import java.util.Scanner;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		String name;
+		int Score=0,answer;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter your name");
+		name = sc.nextLine();
+		
+		System.out.println("hi, "+name+ "welcome to Quiz");
+		System.out.println("Q1.");
+	}
+
+}
